6ac404d30cd7c4953c83d5f192b5418e3fcf9a94
Lonneke <lonnekevanoers@gmail.com>
2025-02-23 14:54:11 +0100

    Completed the challenge

--- js/script.js
+++ js/script.js
@@ -1,16 +1,68 @@
 // A function needs to be created
 function getComputerChoice () {
     // Within said function, create somethign with which you can get a random return
-    computerChoice = Math.floor(Math.random() * 3) + 1
+    let computerChoice = Math.floor(Math.random() * 3) + 1
     // Randomly return either Rock, Paper, or Scissors. 
+    return computerChoice;
+}
 
-    if (computerChoice === 1) {
-        console.log("Rock")
-    } else if (computerChoice === 2) {
-        console.log("Paper")
-    } else {
-        console.log("Scissors")
+// Create the getHumanChoice function
+function getHumanChoice() {
+    // Use prompt to get input
+    let humanAnswer = prompt("What will you play? Rock, Paper, or Scissors?");
+    let text = humanAnswer.toLowerCase();
+    let choice;
+
+    switch (text) {
+        case "rock":
+            choice = 0;
+            break;
+        case "paper":
+            choice = 1;
+            break;
+        case "scissors":
+            choice = 2;
+            break;
+        default:
+            getHumanChoice();
+    }
+    return choice;
+}
+
+// Make sure there are score variables for the Human and the Computer
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice){
+    if(computerChoice == humanChoice) {
+        console.log("Tie!")
+    }
+    else if (computerChoice - 1 == humanChoice || (computerChoice == 0 && humanChoice == 2)) {
+        computerScore++;
+        console.log("The Computer wins this one!!")
+    }
+    else if (humanChoice - 1 == computerChoice || (humanChoice == 0 && computerChoice == 2)) {
+        humanScore++;
+        console.log("The Human wins this one!!")
     }
 }
 
-getComputerChoice()
+function playGame(){
+    for (let i = 0; i < 5; i++)
+    {
+        const computerChoice = getComputerChoice();
+        const humanChoice = getHumanChoice();
+        playRound(humanChoice, computerChoice);
+    }
+    if (computerScore > humanScore) {
+        console.log("The winner of these rounds is  >  Computer");
+    }
+    else if (computerScore < humanScore) {
+        console.log("The winner of these rounds is  >  Human");
+    }
+    else{
+        console.log("That's what we call a tie, folks!");
+    }
+}
+
+playGame();
